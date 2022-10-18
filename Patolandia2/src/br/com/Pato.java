6ab2fe0173636b61f87a5492972d6cc46e13807e
davi.bernardo <davi.bernardo@gw-n112.cefetsc.edu.br>
2022-10-18 17:31:05 -0300

    reestruturação do projeto

--- Patolandia2/src/br/com/Pato.java
+++ Patolandia2/src/br/com/Pato.java
@@ -1,15 +1,28 @@
 package br.com;
 
 public class Pato {
-
+	Voador voador;
+	Grasnador grasnador;
+	
 	public Pato() {}
 	
 	public void nadar() {
 		System.out.println("Estou nadando...");
-		System.out.println("testando o git");
 	}
 	
 	public void display() {
 		System.out.println("Oi, eu sou um pato!");           
 	}
+	
+	public void setVoador(Voador voador) {
+		this.voador = voador;
+		
+	}
+	
+	public void setGrasnador(Grasnador grasnador) {
+		this.grasnador = grasnador;
+	}
+	
+	public void realizarQuack() {}
+	public void realizarVoo() {}
 }
